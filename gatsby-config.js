0c2f7e860430d7ab5946acfe10961c9096c5ef68
Ali Saki <3alisaki@gmail.com>
2021-10-28 19:36:03 +0330

    Setup analytics with google gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,14 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-P8GFWXDY49",
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
