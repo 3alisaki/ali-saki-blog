cbc2a142eefa112a30deeccd2901ebd595731bb9
Ali Saki <3alisaki@gmail.com>
2021-11-03 21:44:45 +0330

    Replace backtick quotes with double quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,59 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
   siteMetadata: {
-    title: `عین`,
-    description: `وبلاگ شخصی علی ساکی.`,
-    siteUrl: `https://alipress.ir/`,
+    title: "عین",
+    description: "وبلاگ شخصی علی ساکی.",
+    siteUrl: "https://alipress.ir/",
   },
   plugins: [
-    `gatsby-plugin-jss`,
-    `gatsby-plugin-image`,
+    "gatsby-plugin-jss",
+    "gatsby-plugin-image",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
+        name: "blog",
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 630,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: "margin-bottom: 1.0725rem",
             },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingIds: [
-          "G-P8GFWXDY49",
-        ],
+        trackingIds: ["G-P8GFWXDY49"],
       },
     },
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: "gatsby-plugin-feed",
       options: {
         query: `
           {
@@ -108,21 +106,21 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `وبلاگ علی ساکی`,
-        short_name: `وبلاگ علی ساکی`,
-        start_url: `/`,
-        background_color: `#ffffff`,
+        name: "وبلاگ علی ساکی",
+        short_name: "وبلاگ علی ساکی",
+        start_url: "/",
+        background_color: "#ffffff",
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        // theme_color: '#663399',
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png",
       },
     },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-nprogress`,
-    `gatsby-plugin-offline`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-nprogress",
+    "gatsby-plugin-offline",
   ],
 }
